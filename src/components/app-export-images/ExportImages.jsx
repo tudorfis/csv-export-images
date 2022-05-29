7466868bd82f0e1b1831e2bd47c3944ce92558a1
tudorfis <tudor.fis@gmail.com>
2022-05-29 14:43:19 +0300

    changes

--- src/components/app-export-images/ExportImages.jsx
+++ src/components/app-export-images/ExportImages.jsx
@@ -1,14 +1,27 @@
 import { Page } from '@shopify/polaris';
 import ActionList from '/src/components/base/ActionList';
 import SettingToggle from '/src/components/base/SettingToggle';
-import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 export function ExportImages() {
+    const [productCount, setProductCount] = useState(0)
+
+    async function updateProductCount() {
+        const { count } = await fetch("/products-count").then((res) => res.json());
+        setProductCount(count);
+    }
+
+    useEffect(() => {
+        updateProductCount()
+    }, [])
+
     return (
         <Page fullWidth>
             <ActionList />
             <br />
             <SettingToggle />
+            <br />
+            <p>Total products: {productCount}</p>
         </Page>
     )
 }
